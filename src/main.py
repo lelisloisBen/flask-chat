b67c2718da9b1d7e73461b86c195716e247b02c4
lelislois Ben <samirbenzada@gmail.com>
2020-10-24 01:32:01 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -40,4 +40,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    socketio.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
