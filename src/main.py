745925e13d23c03a81543f36d4c368addd1002a0
lelislois Ben <samirbenzada@gmail.com>
2020-10-23 20:30:43 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -1,18 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os
 from flask_cors import CORS
 from utils import APIException
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+
 CORS(app)
+socketio = SocketIO(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 @app.route('/')
-def hello_world():
-    return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+
+
+# @app.route('/')
+# def hello_world():
+#     return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
 
 
 # this only runs if `$ python src/main.py` is executed
