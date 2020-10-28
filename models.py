e2289b5699648fe247c1fca06030d67df2d11143
samirbenzada <samirbenzada@gmail.com>
2020-10-28 10:53:08 -0400

    update

--- models.py
+++ models.py
@@ -11,3 +11,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+
+class Message(db.Model):
+    """ Message model """
+    __tablename__ = "message"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    room = db.Column(db.String(10), nullable=False)
+    sent_date = db.Column(db.DateTime, nullable=False)
+    read_flag = db.Column(db.Integer, nullable=False)
