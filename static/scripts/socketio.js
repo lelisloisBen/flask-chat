fd439526207addf54fb7921cfe6e05d3f75dfffa
samirbenzada <samirbenzada@gmail.com>
2020-10-29 11:12:48 -0400

    update

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -42,6 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Timestamp
                     span_timestamp.setAttribute("class", "timestamp");
                     span_timestamp.innerText = data.time_stamp;
+                    
+                    document.querySelector('#display-message-section').innerHTML = AllMessage;
 
                     // HTML to append
                     p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML
