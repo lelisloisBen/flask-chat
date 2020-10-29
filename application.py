10a9bdf6ccc6dc2e773e809b78768e11959b71f0
samirbenzada <samirbenzada@gmail.com>
2020-10-28 23:43:32 -0400

    update

--- application.py
+++ application.py
@@ -105,7 +105,8 @@ def toDb():
     room = data["room"]
     # Set timestamp
     # time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
-    mess = Message(user_name=username, message=msg, room=room, sent_date=time.localtime())
+    myTime = time.time()
+    mess = Message(user_name=username, message=msg, room=room, sent_date=myTime)
     db.session.add(mess)
     db.session.commit()
 
